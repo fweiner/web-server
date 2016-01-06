2916c65240155fe23dab82856b9fe03092a5f7d0
Frederick Weiner <weiner@parrotsoftware.com>
2016-01-06 11:06:32 -0500

    initializecls

--- server.js
+++ server.js
@@ -2,19 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 var d
-var middleware = {
-requireAuthentication: function(req, res, next){
-	
-	console.log("private route hit");
-	next();
-	},
-	logger: function (req, res, next){
-		d = new Date().toString();
-		console.log("Request: " + req.method + " " + req.originalUrl + "Date: " + d);
-		next();
-	}
-};
-// app.use(middleware.requireAuthentication); //at the top application level
+var middleware require("middleware");
+
 
 app.use(middleware.logger);
 app.get("/about", middleware.requireAuthentication, function(req, res){
@@ -22,7 +11,7 @@ res.send("Hello to us!");
 });
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(./middleware);
 app.listen(PORT, function (){
 	console.log("Express Server Started on port " + PORT)
 });
